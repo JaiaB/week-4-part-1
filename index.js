e8efdd7808e66b211885c024c7c76e38afc6f79a
Jaia B <konbae@ymail.com>
2022-12-30 00:43:57 -0500

    creating r repo and adding files

---
+++ index.js
@@ -0,0 +1,12 @@
+function greeting(name, height, country) {
+    alert(
+        "Hello, I am  " +
+            name +
+            ", my height is " +
+            height +
+            "cm and I am from " +
+            country
+    );
+}
+
+greeting("Jaia", 167, "Colombia");
